368328f3056b7dc60002540a0085efae03911801
IrhilOussama <oussama.irhil@usmba.ac.ma>
2024-11-20 23:56:49 +0100

    edit for hosting

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,11 +4,11 @@ module.exports = {
       remotePatterns: [
         {
           // protocol: 'http',
-          protocol: 'http',
-          hostname: 'localhost',
-          // hostname: 'ai-powered-ecomerce.onrender.com',
-          // port: '',
-          port: '5000',
+          protocol: 'https',
+          // hostname: 'localhost',
+          hostname: 'ai-powered-ecomerce.onrender.com',
+          port: '',
+          // port: '5000',
           pathname: '/images/**',
         },
       ],
